b89fc1a80f459176dcb72871798bc538b6c5170d
PowerOfCreation <questerdesura@gmail.com>
2021-04-15 23:32:31 +0200

    dynamic screen boundaries to handle fullscreen

--- Assets/__Game/ScreenBoundary/ScreenBoundary.cs
+++ Assets/__Game/ScreenBoundary/ScreenBoundary.cs
@@ -6,8 +6,24 @@ public class ScreenBoundary : MonoBehaviour
 {
     public static Vector2 screenBoundary;
 
+    private static Vector2 screenResolution;
+
     void Start()
     {
+        UpdateScreenBoundary();
+    }
+
+    private void UpdateScreenBoundary()
+    {
+        screenResolution = new Vector2(Screen.width, Screen.height);
         screenBoundary = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+    }
+
+    void Update()
+    {
+        if(screenResolution.x != Screen.width || screenResolution.y != Screen.height)
+        {
+            UpdateScreenBoundary();
+        }
     }
 }
